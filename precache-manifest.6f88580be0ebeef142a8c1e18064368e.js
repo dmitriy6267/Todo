59ae7de5a4049da067a745fa3f2b146dfecff182
dmitriy6267 <dmitriy6267@gmail.com>
2020-03-18 19:09:51 +0800

    Updates

---
+++ precache-manifest.6f88580be0ebeef142a8c1e18064368e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "210218d0827489e5a1115f4354917a5e",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "38b412b1cadd33c86980",
+    "url": "/Todo/static/css/main.97f291b2.chunk.css"
+  },
+  {
+    "revision": "ad5f31dddd0d514acbca",
+    "url": "/Todo/static/js/2.d416d9a6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Todo/static/js/2.d416d9a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38b412b1cadd33c86980",
+    "url": "/Todo/static/js/main.307f2adf.chunk.js"
+  },
+  {
+    "revision": "4f08a680617c00413cc3",
+    "url": "/Todo/static/js/runtime-main.a575cee7.js"
+  }
+]);
